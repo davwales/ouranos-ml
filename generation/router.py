57ca03d54f65008343b2e9a37651992984b3064e
David Wales <david.wales95@gmail.com>
2023-12-16 11:49:15 -0600

    commented out printlines

--- generation/router.py
+++ generation/router.py
@@ -21,14 +21,14 @@ generator_factory = GeneratorFactory(12, [
 
 @router.post("/text")
 def text(request: TextGenerationRequest):
-    print("Generating a completion...")
+    # print("Generating a completion...")
     generator: PipelineTextGenerator = generator_factory.get_generator(request.type)
     tokens, content = generator.generate(request.context, request.instructions, request.response_start, request.extra_stop_words)
     return TextGenerationResponse(type=request.type, tokens=tokens, content=content)
 
 @router.post("/image")
 def image(request: ImageGenerationRequest):
-    print("Generating an image...")
+    # print("Generating an image...")
     generator: ImageGenerator = generator_factory.get_generator(request.type)
     seed, path = generator.generate(request.prompt, request.negative_prompt, request.width, request.height, request.num_inference_steps, request.seed, request.file_name)
     return ImageGenerationResponse(seed=seed, file_path=path)
@@ -36,5 +36,5 @@ def image(request: ImageGenerationRequest):
 @router.on_event("startup")
 @repeat_every(seconds=60)
 def purge_unused_generators():
-    print("Purging unused generators...")
+    # print("Purging unused generators...")
     generator_factory.purge_unused(seconds=300)
